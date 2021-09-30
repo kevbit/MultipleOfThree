246955635d34f4c382b6cdf841d4882b15419e5d
Kevin Lo <kevin.lo@sjsu.edu>
2021-09-30 13:31:36 -0700

    first commit

---
+++ MultipleOfThree.java
@@ -0,0 +1,22 @@
+/**
+ * Print the sum and product of the first five integers
+ * that are multiple of three.
+ *
+ * @author Kevin Lo 
+ * @version 2020-08-25
+ */
+public class MultipleOfThree
+{
+    public static void main(String[] args)
+    {
+        System.out.print("The sum of 3 + 6 + 9 + 12 + 15 is ");
+        System.out.print(3 + 6 + 9 + 12 + 15);
+        System.out.println(".");
+        System.out.println("");
+        System.out.print("The product of 3 * 6 * 9 * 12 * 15 is ");
+        System.out.print(3 * 6 * 9 * 12 * 15);
+        System.out.println(".");
+
+
+    }
+}
